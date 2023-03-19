93b94ecbc0a820d144306b6f3a4fd91c854ac433
ArslanYM <arsalanyaqmalik@gmail.com>
2023-03-19 20:58:58 +0530

    added like button and like count

---
+++ src/pages/main/main.tsx
@@ -0,0 +1,37 @@
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { useState, useEffect } from "react";
+import { Post } from "./post";
+
+export interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  username: string;
+  description: string;
+}
+
+export const Main = () => {
+  const [postsList, setPostslist] = useState<Post[] | null>(null);
+
+  const postsRef = collection(db, "posts");
+
+  const getPosts = async () => {
+    const data = await getDocs(postsRef);
+    setPostslist(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+    );
+  };
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <div>
+      {postsList?.map((post) => (
+        <Post 
+        post={post}
+        />  
+      ))}
+    </div>
+  );
+};
